4a282e545b8a3f842a06c95dd5bcaf36fb20ece5
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-06 14:19:03 -0400

    Install dependencies

--- src/inspect_flow/_runner/runner.py
+++ src/inspect_flow/_runner/runner.py
@@ -11,15 +11,37 @@ from inspect_flow._types.types import TaskGroupConfig
 async def run(task_group: TaskGroupConfig) -> None:
     temp_dir_parent: pathlib.Path = pathlib.Path.home() / ".cache" / "inspect-flow"
     temp_dir_parent.mkdir(parents=True, exist_ok=True)
-    with tempfile.TemporaryDirectory(dir=temp_dir_parent) as temp_dir:
+    with tempfile.TemporaryDirectory(dir=temp_dir_parent, delete=False) as temp_dir:
         shutil.copy2(
             Path(task_group.pyproject_toml_file), Path(temp_dir) / "pyproject.toml"
         )
         if task_group.uv_lock_file:
             shutil.copy2(Path(task_group.uv_lock_file), Path(temp_dir) / "uv.lock")
 
+        # subprocess.run(
+        #     ["uv", "venv"],
+        #     cwd=temp_dir,
+        #     check=True,
+        #     capture_output=True,
+        #     text=True,
+        #     env=os.environ.copy(),
+        # )
+
         subprocess.run(
             ["uv", "sync", "--no-install-project"],
+            cwd=temp_dir,
+            check=True,
+            capture_output=True,
+            text=True,
+            env=os.environ.copy(),
+        )
+
+        eval_set_config = task_group.eval_set
+        package_configs = [*eval_set_config.tasks, *(eval_set_config.models or [])]
+        dependencies = {*(package_config.package for package_config in package_configs)}
+
+        subprocess.run(
+            ["uv", "pip", "install", *sorted(dependencies)],
             cwd=temp_dir,
             check=True,
             capture_output=True,
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,18 +1,13 @@
 from pathlib import Path
 
 import pytest
+from inspect_flow._config.config import load_config
 from inspect_flow._runner.runner import run
 from inspect_flow._types.types import EvalSetConfig, TaskGroupConfig
 
 
 @pytest.mark.asyncio
 async def test_run_environment() -> None:
-    pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
-    uv_lock_file = Path(__file__).parent.parent / "uv.lock"
-
-    task_group = TaskGroupConfig(
-        pyproject_toml_file=str(pyproject_toml_file),
-        uv_lock_file=str(uv_lock_file),
-        eval_set=EvalSetConfig(tasks=[]),
-    )
-    await run(task_group=task_group)
+    config_path = Path(__file__).parent.parent / "examples" / "simple.eval-set.yaml"
+    config = load_config(str(config_path))
+    await run(task_group=config.run.task_groups[0])
