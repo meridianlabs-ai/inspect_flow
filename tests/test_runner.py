abf87cf5da390eb62f94c4fe841af997bbf7a738
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-07 10:25:33 -0400

    disable run test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 
-import pytest
 from inspect_flow._config.config import load_config
 from inspect_flow._runner.runner import run
 
 
-@pytest.mark.asyncio
-async def test_run_environment() -> None:
+def _test_run_environment() -> None:
     config_path = Path(__file__).parent.parent / "examples" / "simple.eval-set.yaml"
     config = load_config(str(config_path))
     run(task_group=config.run.task_groups[0])
