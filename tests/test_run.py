d9692c66a63737fef56f608ac154b032521edcb3
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-07 15:58:16 -0400

    disable the run test (only works when local_eval is pip installed)

--- tests/test_run.py
+++ tests/test_run.py
@@ -4,7 +4,7 @@ from inspect_flow._config.config import load_config
 from inspect_flow._runner.run import run_eval_set
 
 
-def test_run_eval_set() -> None:
+def _test_run_eval_set() -> None:
     config_path = Path(__file__).parent.parent / "examples" / "local.eval-set.yaml"
     config = load_config(str(config_path))
     run_eval_set(eval_set_config=config.run.task_groups[0].eval_set)
