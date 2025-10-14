e46c68ba37f3da78ca711933aed6594a9bd76b96
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-14 10:32:16 -0400

    Support get_model args

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from unittest.mock import patch
 
 from inspect_ai import Task
+from inspect_ai.model import GenerateConfig, Model
 from inspect_flow._runner.run import run_eval_set
 from inspect_flow._types.types import (
     EvalSetConfig,
+    GetModelArgs,
     ModelConfig,
     PackageConfig,
     TaskConfig,
@@ -44,13 +46,8 @@ def test_task_with_get_model() -> None:
             )
         )
 
-        # Verify eval_set was called once
         mock_eval_set.assert_called_once()
-
-        # Get the call arguments
         call_args = mock_eval_set.call_args
-
-        # Verify the first positional argument is a single Task object
         tasks_arg = call_args.kwargs["tasks"]
         assert len(tasks_arg) == 1
         assert isinstance(tasks_arg[0], Task)
@@ -88,3 +85,43 @@ def test_task_with_two_models() -> None:
     run_eval_set(eval_set_config=eval_set_config)
 
     verify_test_logs(eval_set_config, log_dir)
+
+
+def test_model_generate_config() -> None:
+    system_message = "Test System Message"
+    with patch("inspect_ai.eval_set") as mock_eval_set:
+        run_eval_set(
+            eval_set_config=EvalSetConfig(
+                tasks=[
+                    PackageConfig(
+                        name="local_eval",
+                        file=str(task_file.absolute()),
+                        items=[TaskConfig(name="noop")],
+                    )
+                ],
+                log_dir="get_model",
+                models=[
+                    PackageConfig(
+                        name="mockllm",
+                        package="mockllm",
+                        items=[
+                            ModelConfig(
+                                name="mock-llm",
+                                args=GetModelArgs(
+                                    config=GenerateConfig(system_message=system_message)
+                                ),
+                            )
+                        ],
+                    )
+                ],
+            )
+        )
+
+        mock_eval_set.assert_called_once()
+        call_args = mock_eval_set.call_args
+        tasks_arg = call_args.kwargs["tasks"]
+        assert len(tasks_arg) == 1
+        assert isinstance(tasks_arg[0], Task)
+        assert isinstance(tasks_arg[0].model, Model)
+        config = tasks_arg[0].model.config
+        assert config.system_message == system_message
