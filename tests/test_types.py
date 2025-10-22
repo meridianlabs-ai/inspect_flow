4e89f41fa5b3104714e3026aefa4156fc1b3b851
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-22 12:13:11 -0400

    better __init__ signature

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,21 +1,23 @@
 from __future__ import annotations
 
-from inspect_flow._types.flow_type_dicts import MatrixDict
-from inspect_flow._types.flow_types import FlowConfig, Matrix, TaskConfig
+from inspect_flow._types.flow_types import FlowConfig, Matrix, MatrixDict, TaskConfig
 
 matrix: MatrixDict = {"tasks": []}
-matrix: MatrixDict = {}
 
 config = FlowConfig({"matrix": [{"tasks": ["one_module/one_task"]}]})
-config = FlowConfig({"matrix": [Matrix(tasks=[Matrix(tasks=[])])]})
-config = FlowConfig({"matrix": [TaskConfig()]})
 config = FlowConfig(matrix=[{"tasks": []}])
-config = FlowConfig(matrix=[Matrix(tasks=[]), TaskConfig()])
 config = FlowConfig(matrix=[Matrix(tasks=[{"name": "dict"}])])
 config = FlowConfig(
     matrix=[Matrix(tasks=["string", TaskConfig(name="class"), {"name": "dict"}])]
 )
 config = FlowConfig(matrix=[Matrix(tasks=[])])
+
+# Should fail
+matrix: MatrixDict = {}
+matrix = Matrix({"tasks": []})
+config = FlowConfig({"matrix": [Matrix(tasks=[Matrix(tasks=[])])]})
+config = FlowConfig({"matrix": [TaskConfig()]})
+config = FlowConfig(matrix=[Matrix(tasks=[]), TaskConfig()])
 config = FlowConfig(bad=None)
 
 
