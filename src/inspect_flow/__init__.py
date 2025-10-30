d30d5885eb1502f3dc6c86ae65ab9c6269802b13
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-30 16:26:33 -0400

    Add missing exports

--- src/inspect_flow/__init__.py
+++ src/inspect_flow/__init__.py
@@ -5,19 +5,27 @@ except ImportError:
 
 from inspect_flow._submit.submit import submit
 from inspect_flow._types.factories import (
+    configs,
     flow_agent,
     flow_config,
     flow_model,
     flow_solver,
     flow_task,
+    models,
+    solvers,
+    tasks,
 )
 
 __all__ = [
     "__version__",
+    "configs",
     "flow_agent",
     "flow_config",
     "flow_model",
     "flow_solver",
     "flow_task",
+    "models",
+    "solvers",
     "submit",
+    "tasks",
 ]
