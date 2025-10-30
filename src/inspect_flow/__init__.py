ec5e5df06ab2062746d18520567d3384fec45e82
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-30 16:23:39 -0400

    split __init__

--- src/inspect_flow/__init__.py
+++ src/inspect_flow/__init__.py
@@ -4,22 +4,6 @@ except ImportError:
     __version__ = "unknown"
 
 from inspect_flow._submit.submit import submit
-from inspect_flow._types.dicts import (
-    ApprovalPolicyConfigDict,
-    ApproverPolicyConfigDict,
-    BatchConfigDict,
-    FlowAgentDict,
-    FlowConfigDict,
-    FlowEpochsDict,
-    FlowModelDict,
-    FlowOptionsDict,
-    FlowSolverDict,
-    FlowTaskDict,
-    GenerateConfigDict,
-    JSONSchemaDict,
-    ResponseSchemaDict,
-    SandboxEnvironmentSpecDict,
-)
 from inspect_flow._types.factories import (
     flow_agent,
     flow_config,
@@ -27,39 +11,9 @@ from inspect_flow._types.factories import (
     flow_solver,
     flow_task,
 )
-from inspect_flow._types.flow_types import (
-    FlowAgent,
-    FlowConfig,
-    FlowEpochs,
-    FlowModel,
-    FlowOptions,
-    FlowSolver,
-    FlowTask,
-)
 
 __all__ = [
     "__version__",
-    "ApprovalPolicyConfigDict",
-    "ApproverPolicyConfigDict",
-    "BatchConfigDict",
-    "FlowAgent",
-    "FlowAgentDict",
-    "FlowConfig",
-    "FlowConfigDict",
-    "FlowEpochs",
-    "FlowEpochsDict",
-    "FlowModel",
-    "FlowModelDict",
-    "FlowOptions",
-    "FlowOptionsDict",
-    "FlowSolver",
-    "FlowSolverDict",
-    "FlowTask",
-    "FlowTaskDict",
-    "GenerateConfigDict",
-    "JSONSchemaDict",
-    "ResponseSchemaDict",
-    "SandboxEnvironmentSpecDict",
     "flow_agent",
     "flow_config",
     "flow_model",
