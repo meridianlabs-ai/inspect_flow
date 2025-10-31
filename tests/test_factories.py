27945ef2225c4510c0fc9e595055e4231f6b2cf3
Ransom Richardson <ransom@meridianlabs.ai>
2025-10-31 18:04:15 -0400

    fix tests

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -23,8 +23,7 @@ def test_tasks_x_models():
 
 def test_flow_task_x_models():
     result = tasks(
-        FlowTask(name="task1"),
-        matrix={"model": ["model1", "model2"]},
+        matrix={"task": FlowTask(name="task1"), "model": ["model1", "model2"]},
     )
     assert len(result) == 2
     assert result[0].name == "task1"
@@ -35,22 +34,35 @@ def test_flow_task_x_models():
     assert result[1].model.name == "model2"
 
 
+def test_task_x_names():
+    result = tasks(
+        matrix={"task": {"model": "model1"}, "name": ["task1", "task2"]},
+    )
+    assert len(result) == 2
+    assert result[0].name == "task1"
+    assert result[0].model
+    assert result[0].model.name == "model1"
+    assert result[1].name == "task2"
+    assert result[1].model
+    assert result[1].model.name == "model1"
+
+
 def test_duplicate_raises():
     with pytest.raises(ValueError, match="name provided in both base and matrix"):
-        tasks(FlowTask(name="task1"), matrix={"name": ["task2"]})
+        tasks(matrix={"task": FlowTask(name="task1"), "name": ["task2"]})
 
 
 def test_nested_types():
     result = tasks(
-        FlowTask(name="task1"),
         matrix={
+            "task": FlowTask(name="task1"),
             "model": [
                 {
                     "name": "model1",
                     "config": {"system_message": "test system message"},
                 },
                 {"name": "model2"},
-            ]
+            ],
         },
     )
     assert len(result) == 2
@@ -65,23 +77,23 @@ def test_nested_types():
 def test_nested_types_error():
     with pytest.raises(ValidationError):
         tasks(
-            FlowTask(name="task1"),
             matrix={
+                "task": FlowTask(name="task1"),
                 "model": [
                     {
                         "name": "model1",
                         "config": [{"system_message": "test system message"}],
                     },  # type: ignore
                     {"name": "model2"},
-                ]
+                ],
             },
         )
 
 
 def test_configs():
     result = tasks(
-        FlowTask(name="task1", model=FlowModel(name="model1")),
         matrix={
+            "task": FlowTask(name="task1", model=FlowModel(name="model1")),
             "config": configs(matrix={"system_message": ["message1", "message2"]}),
         },
     )
