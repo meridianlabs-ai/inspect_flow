bb43d6ada1cf909df45b085069979bba3aaf8d6f
Ransom Richardson <ransom@meridianlabs.ai>
2025-11-07 14:45:38 -0500

    Use cli in e2e test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,17 +1,29 @@
 from pathlib import Path
 
+from click.testing import CliRunner
+from inspect_flow._cli.run import run_command
 from inspect_flow._config.config import load_config
-from inspect_flow._launcher.launch import launch
 
 from tests.test_helpers.log_helpers import init_test_logs, verify_test_logs
 
 
 def test_local_e2e() -> None:
-    log_dir = init_test_logs()
+    flow_dir = init_test_logs()
 
     config_path = Path(__file__).parent / "config" / "e2e_test_flow.py"
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        run_command,
+        [
+            str(config_path),
+            "--set",
+            f"flow_dir={flow_dir}",
+        ],
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
     config = load_config(str(config_path))
-
-    launch(config=config, config_file_path=str(config_path))
-
-    verify_test_logs(config=config, log_dir=log_dir)
+    verify_test_logs(config=config, log_dir=flow_dir)
