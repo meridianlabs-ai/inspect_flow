b860194d323ade8088b31220f16c70033ad8acaf
Ransom Richardson <ransom@meridianlabs.ai>
2025-11-20 13:12:34 -0500

    Test for #220 (#230)

--- tests/config/auto/_flow.py
+++ tests/config/auto/_flow.py
@@ -1,16 +1,13 @@
 from inspect_flow import FlowDefaults, FlowGenerateConfig, FlowJob, FlowModel
 
 FlowJob(
+    includes=["_other_flow.py"],
     defaults=FlowDefaults(
         model_prefix={
-            "antropic": FlowModel(
-                base_url="https://api.anthropic.com/v1/",
-                config=FlowGenerateConfig(max_connections=2),
-            ),
             "openai": FlowModel(
                 base_url="https://api.openai.com/v1/",
                 config=FlowGenerateConfig(max_connections=5),
             ),
         }
-    )
+    ),
 )
