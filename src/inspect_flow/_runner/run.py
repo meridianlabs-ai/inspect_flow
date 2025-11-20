80b27b8b871ea8e80a63143158f33a74949b4df0
Ransom Richardson <ransom@meridianlabs.ai>
2025-11-20 11:56:09 -0500

    #217 fix error message to specify 'bundle_overwrite' (#224)
    * #217 fix error message to specify 'bundle_overwrite' instead of 'overwrite'
    
    * do not replace message unless it changed

--- src/inspect_flow/_runner/run.py
+++ src/inspect_flow/_runner/run.py
@@ -2,6 +2,7 @@ import click
 import inspect_ai
 import yaml
 from inspect_ai._util.dateutil import iso_now
+from inspect_ai._util.error import PrerequisiteError
 from inspect_ai._util.file import clean_filename_component, file
 from inspect_ai.log import EvalLog
 
@@ -47,63 +48,81 @@ def _run_eval_set(job: FlowJob, dry_run: bool = False) -> tuple[bool, list[EvalL
 
     _write_config_file(resolved_config)
 
-    result = inspect_ai.eval_set(
-        tasks=tasks,
-        log_dir=resolved_config.log_dir,
-        retry_attempts=options.retry_attempts,
-        retry_wait=options.retry_wait,
-        retry_connections=options.retry_connections,
-        retry_cleanup=options.retry_cleanup,
-        # model= FlowTask
-        # model_base_url= FlowModel
-        # model_args= FlowModel
-        # model_roles= FlowTask
-        # task_args= FlowTask
-        sandbox=options.sandbox,
-        sandbox_cleanup=options.sandbox_cleanup,
-        # solver= FlowTask
-        tags=options.tags,
-        metadata=options.metadata,
-        trace=options.trace,
-        display=options.display,
-        approval=options.approval,
-        score=options.score or True,
-        log_level=options.log_level,
-        log_level_transcript=options.log_level_transcript,
-        log_format=options.log_format,
-        limit=options.limit,
-        # sample_id= FlowTask
-        sample_shuffle=options.sample_shuffle,
-        # epochs= FlowTask
-        fail_on_error=options.fail_on_error,
-        continue_on_fail=options.continue_on_fail,
-        retry_on_error=options.retry_on_error
-        if options.retry_on_error is not None
-        else 3,
-        debug_errors=options.debug_errors,
-        # message_limit= FlowTask
-        # token_limit= FlowTask
-        # time_limit= FlowTask
-        # working_limit= FlowTask
-        max_samples=options.max_samples,
-        max_tasks=options.max_tasks if options.max_tasks is not None else 10,
-        max_subprocesses=options.max_subprocesses,
-        max_sandboxes=options.max_sandboxes,
-        log_samples=options.log_samples,
-        log_realtime=options.log_realtime,
-        log_images=options.log_images,
-        log_buffer=options.log_buffer,
-        log_shared=options.log_shared,
-        bundle_dir=options.bundle_dir,
-        bundle_overwrite=options.bundle_overwrite or False,
-        log_dir_allow_dirty=options.log_dir_allow_dirty,
-        # kwargs= FlowJob, FlowTask, and FlowModel allow setting the generate config
-    )
+    try:
+        result = inspect_ai.eval_set(
+            tasks=tasks,
+            log_dir=resolved_config.log_dir,
+            retry_attempts=options.retry_attempts,
+            retry_wait=options.retry_wait,
+            retry_connections=options.retry_connections,
+            retry_cleanup=options.retry_cleanup,
+            # model= FlowTask
+            # model_base_url= FlowModel
+            # model_args= FlowModel
+            # model_roles= FlowTask
+            # task_args= FlowTask
+            sandbox=options.sandbox,
+            sandbox_cleanup=options.sandbox_cleanup,
+            # solver= FlowTask
+            tags=options.tags,
+            metadata=options.metadata,
+            trace=options.trace,
+            display=options.display,
+            approval=options.approval,
+            score=options.score or True,
+            log_level=options.log_level,
+            log_level_transcript=options.log_level_transcript,
+            log_format=options.log_format,
+            limit=options.limit,
+            # sample_id= FlowTask
+            sample_shuffle=options.sample_shuffle,
+            # epochs= FlowTask
+            fail_on_error=options.fail_on_error,
+            continue_on_fail=options.continue_on_fail,
+            retry_on_error=options.retry_on_error
+            if options.retry_on_error is not None
+            else 3,
+            debug_errors=options.debug_errors,
+            # message_limit= FlowTask
+            # token_limit= FlowTask
+            # time_limit= FlowTask
+            # working_limit= FlowTask
+            max_samples=options.max_samples,
+            max_tasks=options.max_tasks if options.max_tasks is not None else 10,
+            max_subprocesses=options.max_subprocesses,
+            max_sandboxes=options.max_sandboxes,
+            log_samples=options.log_samples,
+            log_realtime=options.log_realtime,
+            log_images=options.log_images,
+            log_buffer=options.log_buffer,
+            log_shared=options.log_shared,
+            bundle_dir=options.bundle_dir,
+            bundle_overwrite=options.bundle_overwrite or False,
+            log_dir_allow_dirty=options.log_dir_allow_dirty,
+            # kwargs= FlowJob, FlowTask, and FlowModel allow setting the generate config
+        )
+    except PrerequisiteError as e:
+        _fix_prerequisite_error_message(e)
+        raise
 
     if result[0]:
         _print_bundle_url(resolved_config)
 
     return result
+
+
+def _fix_prerequisite_error_message(e: PrerequisiteError) -> None:
+    # Issue #217 - update error message to use 'bundle_overwrite' instead of 'overwrite'
+    original_message = str(e.message)
+    modified_message = original_message.replace("'overwrite'", "'bundle_overwrite'")
+    if original_message != modified_message:
+        e.message = modified_message
+
+    if e.args:
+        original_message = str(e.args[0])
+        modified_message = original_message.replace("'overwrite'", "'bundle_overwrite'")
+        if original_message != modified_message:
+            e.args = (modified_message, *e.args[1:])
 
 
 def _print_bundle_url(job: FlowJob) -> None:
