796754145654fbe89250e97517181bfd6af4a2e7
Ransom Richardson <ransom@meridianlabs.ai>
2025-12-02 10:58:04 -0500

    Fix example

--- examples/dependencies.py
+++ examples/dependencies.py
@@ -2,11 +2,9 @@ from inspect_flow import FlowDependencies, FlowJob, FlowTask
 
 FlowJob(
     dependencies=FlowDependencies(
-        dependency_file_mode="auto",  # <1>
-        dependency_file="../foo/requirements.txt",  # <2>
-        additional_dependencies=["pandas==2.0.0"],  # <3>
-        auto_detect_dependencies=True,  # <4>
-        use_uv_lock=True,  # <5>
+        dependency_file="../foo/requirements.txt",  # <1>
+        additional_dependencies=["pandas==2.0.0"],  # <2>
+        auto_detect_dependencies=True,  # <3>
     ),
     log_dir="logs",
     tasks=[
