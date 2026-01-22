364f7f9708f7fcfff2bbd64a414c918c9b55e81a
Ransom Richardson <ransom@meridianlabs.ai>
2026-01-22 13:24:33 -0500

    test tools and venv

--- tests/local_eval/flow/local_eval_flow.py
+++ tests/local_eval/flow/local_eval_flow.py
@@ -1,5 +1,6 @@
 from inspect_flow import FlowOptions, FlowSpec, FlowTask, tasks_matrix
-from local_eval import noop2
+from inspect_flow._types.flow_types import FlowAgent
+from local_eval import add, my_agent, noop2
 
 FlowSpec(
     options=FlowOptions(limit=1),
@@ -7,7 +8,10 @@ FlowSpec(
         task=[
             "local_eval/noop",  # task from the package
             "../src/local_eval/noop.py@noop",  # task from a file relative to the config
-            FlowTask(factory=noop2),  # task from an imported function
+            FlowTask(
+                factory=noop2,
+                solver=FlowAgent(factory=my_agent, args={"tools": [add()]}),
+            ),  # task from an imported function
         ],
         model=["mockllm/mock-llm1", "mockllm/mock-llm2"],
     ),
--- tests/local_eval/src/local_eval/tools.py
+++ tests/local_eval/src/local_eval/tools.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from inspect_ai.agent import Agent, AgentState, agent
 from inspect_ai.tool import Tool, tool
 
 
@@ -15,5 +18,17 @@ def add() -> Tool:
             The sum of the two numbers.
         """
         return x + y
+
+    return execute
+
+
+@agent
+def my_agent(tools: list[Any]) -> Agent:
+    assert len(tools) == 1
+    assert callable(tools[0])
+    assert tools[0].__qualname__ == add().__qualname__
+
+    async def execute(state: AgentState) -> AgentState:
+        return state
 
     return execute
