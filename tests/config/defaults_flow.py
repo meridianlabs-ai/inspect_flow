6b61a9355b64b71f34be5f91afdd8d07598d236a
Ransom Richardson <ransom@meridianlabs.ai>
2026-01-22 16:26:37 -0500

    fix: enable relative imports for local file

--- tests/config/defaults_flow.py
+++ tests/config/defaults_flow.py
@@ -1,6 +1,6 @@
 from inspect_flow import FlowDefaults, FlowModel, FlowSpec
 
-FlowSpec(
+spec = FlowSpec(
     defaults=FlowDefaults(
         model=FlowModel(name="mockllm/mock-llm1", model_args={"mock_model_arg": 3})
     )
