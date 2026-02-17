79018989a5b09ce664fe998902e1eefd1e223f89
Ransom Richardson <ransom@meridianlabs.ai>
2026-02-17 09:44:36 -0500

    Fix switched import and remove

--- src/inspect_flow/_cli/store.py
+++ src/inspect_flow/_cli/store.py
@@ -168,7 +168,9 @@ def store_import(
     **kwargs: Unpack[StoreOptionArgs],
 ) -> None:
     if dry_run:
-        flow_print("\n[blue][DRY RUN][/blue] Preview mode - logs will not be removed\n")
+        flow_print(
+            "\n[blue][DRY RUN][/blue] Preview mode - logs will not be imported\n"
+        )
 
     flow_store = init_store(create=True, **kwargs)
     if not flow_store:
@@ -230,9 +232,7 @@ def store_remove(
     **kwargs: Unpack[StoreOptionArgs],
 ) -> None:
     if dry_run:
-        flow_print(
-            "\n[blue][DRY RUN][/blue] Preview mode - logs will not be imported\n"
-        )
+        flow_print("\n[blue][DRY RUN][/blue] Preview mode - logs will not be removed\n")
     if not prefix and not missing:
         raise click.UsageError("Either prefix or --missing must be specified.")
     if prefix and missing:
