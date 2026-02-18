0d5d160880ad79b18d87f75ece2c43a5cf6343de
Ransom Richardson <ransom@meridianlabs.ai>
2026-02-18 11:16:14 -0500

    Add DisplayType to API

--- src/inspect_flow/api/__init__.py
+++ src/inspect_flow/api/__init__.py
@@ -1,9 +1,11 @@
 """inspect_flow python API."""
 
 from inspect_flow._api.api import config, init, load_spec, run, store_get
+from inspect_flow._display.display import DisplayType
 from inspect_flow._store.store import FlowStore
 
 __all__ = [
+    "DisplayType",
     "FlowStore",
     "config",
     "init",
